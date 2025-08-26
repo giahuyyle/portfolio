d1bbeb0d4997b413f8e694d1b37c257bb5b1f195
Gia Huy Le <huyyyylegia@gmail.com>
2025-08-27 03:36:35 +0700

    added firebase get list function

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
+
+// firebase app for realtime updates on projects, experiences and other things
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -10,3 +13,12 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export async function getCollection(collectionName) {
+    const col = collection(db, collectionName);
+    const snapshot = await getDocs(col);
+    const list = snapshot.docs.map(doc => doc.data());
+    return list;
+};
